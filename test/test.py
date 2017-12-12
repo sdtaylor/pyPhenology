17fefff29621b79f3def77db5e55a85a9151181e
Shawn Taylor <shawntaylor@weecology.org>
2017-12-12 15:09:10 -0500

    more test cases
    test many different possible fixed and estimatable parameter
    combinations

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 from pyPhenology import utils, models
+import pytest
 
 doy, temp = utils.load_test_data(name='vaccinium')
 
@@ -13,7 +14,37 @@ for model_name, Model in model_list.items():
     model.fit(DOY=doy, temperature=temp, verbose=True)
     model.predict(doy, temp)
 
-    # Test with 1 fixed parameters
-    #model = models.Thermal_Time()
-    #model.fit(doy, temp)
+    # Use estimated parameter values in other tests
+    all_parameters = model.get_params()
     
+    # Test with all parameters set to a fixed value
+    print(model_name + ' - Fix all parameters')
+    model = Model(parameters=all_parameters)
+    model.predict(doy, temp)
+    
+    # Only a single parameter set to fixed
+    print(model_name + ' - Fix a single parameter')
+    param, value = all_parameters.popitem()
+    single_param = {param:value}
+    model = Model(parameters=single_param)
+    model.fit(DOY=doy, temperature=temp, verbose=True)
+    model.predict(doy, temp)
+    assert model.get_params()[param]==value, 'Fixed parameter does not equal final one: '+str(param)
+
+    # Estimate only a single parameter
+    print(model_name + ' - Estimate a single parameter')
+    # all_parameters is now minus one due to popitem()
+    model = Model(parameters=all_parameters)
+    model.fit(DOY=doy, temperature=temp, verbose=True)
+    model.predict(doy, temp)
+    for param, value in all_parameters.items():
+        assert model.get_params()[param] == value, 'Fixed parameter does not equal final one: '+str(param)
+    
+    # Expect error when a bogus parameter gets passed
+    print(model_name + ' - Should not accept unknown parameters')
+    with pytest.raises(AssertionError) as a:
+        model = Model(parameters={'not_a_parameter':0})
+
+# Make sure some known parameters are estimated correctly
+#fixed_gdd_model = models.Thermal_Time(parameters={'t1':0, 'T':0})
+#fixed_gdd_model.fit(DOY=doy, temperature=temp, verbose=True)
