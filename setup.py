ac1709ab283bc3526d2794869e28b8df441c27be
Shawn Taylor <shawntaylor@weecology.org>
2017-12-15 14:58:54 -0500

    take email out

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyPhenology',
+      version='0.0.1',
+      description='Plant phenology models in python',
+      url='https://github.com/sdtaylor/pyPhenology',
+      author='Shawn Taylor',
+      license='MIT',
+      packages=['pyPhenology'],
+      zip_safe=False)
