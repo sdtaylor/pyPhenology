97467d28a798eca748cc68e8ee39f37fbedbe732
Shawn Taylor <shawntaylor@weecology.org>
2018-02-11 11:42:54 -0500

    add more models and save

--- test/time_model_fitting.py
+++ test/time_model_fitting.py
@@ -3,7 +3,7 @@ observations, temp = utils.load_test_data(name='vaccinium')
 import pandas as pd
 import time
 
-models_to_use = ['ThermalTime','Alternating','Uniforc','Unichill']
+models_to_use = ['ThermalTime','Alternating','Uniforc','MSB','Unichill']
 methods_to_use = ['DE','BF']
 sensible_defaults = ['testing','practical','intensive']
 
@@ -14,10 +14,19 @@ for m in models_to_use:
             model = utils.load_model(m)()
             
             start_time = time.time()
-            m.fit(observations, temp, method=optimizer_method, optimizer_params=s_default)
+            model.fit(observations, temp, method=optimizer_method, optimizer_params=s_default)
             fitting_time = round(time.time() - start_time,2)
             
+            num_parameters = len(model.get_params())
+            
             timings.append({'model':m,
+                            'num_parameters':num_parameters,
                             'method':optimizer_method,
                             'sensible_default':s_default,
                             'time':fitting_time})
+            
+            print(timings[-1])
+            print('#######################')
+    
+timings = pd.DataFrame(timings)
+timings.to_csv('model_fit_timings.csv')
