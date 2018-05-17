4f54e71008db3cee4cb5a3b4246091ff46ff8b12
Shawn Taylor <sdtaylor@users.noreply.github.com>
2018-05-17 16:41:23 -0400

    Fall cooling model (#82)
    for modelling fall leaf senescence

--- pyPhenology/models/thermaltime.py
+++ pyPhenology/models/thermaltime.py
@@ -14,6 +14,11 @@ class ThermalTime(BaseModel):
     .. math::
         \sum_{t=t_{1}}^{DOY}R_{f}(T_{i})\geq F^{*}
 
+    where:
+
+    .. math::
+        R_{f}(T_{i}) = max(T_{i}-threshold, 0)
+
     Parameters:
         t1 : int
             | :math:`t_{1}` - The DOY which forcing accumulating beings
@@ -49,7 +54,6 @@ class ThermalTime(BaseModel):
                                               doy_series=doy_series,
                                               threshold=F)
 
-
 class M1(BaseModel):
     """The Thermal Time Model with a daylength correction.
 
@@ -58,6 +62,11 @@ class M1(BaseModel):
     .. math::
 
         \sum_{t=t_{1}}^{DOY}R_{f}(T_{i}) \geq (L_{i}/24)^kF^*
+
+    where:
+
+    .. math::
+        R_{f}(T_{i}) = max(T_{i}-threshold, 0)
 
     This model requires a daylength column in the predictors in
     addition to daily mean temperature.
@@ -136,6 +145,68 @@ class M1(BaseModel):
         # Make it the same shape as gdd for easy adjustment
         num_days = len(doy_series)
         daylength_adjustment = np.tile(np.expand_dims(daylength_adjustment, 1), num_days)
+
+        return utils.transforms.doy_estimator(forcing=accumulated_gdd,
+                                              doy_series=doy_series,
+                                              threshold=F)
+
+class FallCooling(BaseModel):
+    """Fall senesence model
+
+    A model for fall senesence. Essential a Thermal Time model,
+    but instead of accumulating warming above a base temperature,
+    it accumulates cooling below a max temperature. 
+
+    Event happens on :math:`DOY` when the following is met:
+
+    .. math::
+        \sum_{t=t_{1}}^{DOY}R_{f}(T_{i})\geq F^{*}
+
+    where:
+
+    .. math::
+        R_{f}(T_{i}) = max(threshold-T_{i}, 0)
+    
+    This is a simplified version of the model in Delpierre et al. 2009.
+    The full version also has a photoperiod compoenent. 
+
+    Parameters:
+        t1 : int
+            | :math:`t_{1}` - The DOY which forcing accumulating beings
+            | default : (182,365)
+
+        T : int
+            | :math:`T` - The threshold below which cooling accumulates
+            | default : (-25,25)
+
+        F : int, > 0
+            | :math:`F^{*}` - The total cooling units required
+            | default : (0,1000)
+
+    Notes:
+        Delpierre, N., Dufrêne, E., Soudani, K., Ulrich, E., Cecchini, S., Boé,
+        J., & François, C. (2009). Modelling interannual and spatial
+        variability of leaf senescence for three deciduous tree species in
+        France. Agricultural and Forest Meteorology, 149(6–7), 938–948.
+        http://doi.org/10.1016/j.agrformet.2008.11.014
+
+    """
+
+    def __init__(self, parameters={}):
+        BaseModel.__init__(self)
+        self.all_required_parameters = {'t1': (182, 365), 'T': (-25, 25), 'F': (0, 1000)}
+        self._organize_parameters(parameters)
+        self._required_data = {'predictor_columns': ['site_id', 'year', 'doy', 'temperature'],
+                               'predictors': ['temperature', 'doy_series']}
+
+    def _apply_model(self, temperature, doy_series, t1, T, F):
+        # Temperature threshold
+        temperature[temperature > T] = 0
+
+        # Only accumulate forcing after t1
+        temperature[doy_series < t1] = 0
+
+        accumulated_gdd = utils.transforms.forcing_accumulator(temperature)
 
         return utils.transforms.doy_estimator(forcing=accumulated_gdd,
                                               doy_series=doy_series,
