be120bf90094322d977189b7f47a673b27460827
Shawn <shawntaylor@weecology.org>
2018-07-03 15:25:02 -0400

    0.5.0 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyPhenology',
-      version='0.0.1',
+      version='0.5.0',
       description='Plant phenology models in python',
       url='https://github.com/sdtaylor/pyPhenology',
       author='Shawn Taylor',
